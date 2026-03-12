3c1e3e7446d236af43ea4d826b273aec53f1d670
de-SchrijverS <de-schrijvers@hpc-login01.rki.local>
2026-03-12 17:17:30 +0100

    initialized statisticalcomparator

---
+++ src/evaluation/statisticalcomparator.py
@@ -0,0 +1,10 @@
+"""
+A statistical-testing class should come here:
+
+- diebald-Mariano
+- Wilcoxon signed - rank
+- Friedman test + Nemenyi post-hoc
+- mannwhitneyu
+- kruskal
+- spatial autocorrelation?
+"""
