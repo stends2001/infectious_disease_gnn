06169737852fcf258d44b639e23c21b7ac682837
de-SchrijverS <de-schrijvers@hpc-login01.rki.local>
2026-04-09 10:09:16 +0200

    renamed module and updated imports of colregistry and containers

---
+++ src/dataloading/epidataorchestration/validators/base.py
@@ -0,0 +1,47 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Any, Union
+import pandas as pd
+
+from .issues import UnexpectedAttributeTypeError, EmptyAttributeTypeError
+from ...epiconfig.epiconfig import EpiConfig
+
+class EpiDataContainerValidator(ABC):
+    """ 
+    Parent class to all validators
+    Subclasses may have any helper methods, but must
+    have one centralized `.validate()` method that 
+    orchestrates and runs the validation.
+
+    Some methods are shared and therefore defined in this parent
+    class. This includes:
+    - `validate_type()`
+    - `validate_length_nonzero()`
+    """
+
+    def __init__(self,
+                epiconfig:              'EpiConfig',
+                dataclass_validated:    str):
+        
+        self.epiconfig          =  epiconfig
+        self.dataclass_validated= dataclass_validated
+
+        self.allowed_types   = (pd.DataFrame, Dict)
+
+    @abstractmethod
+    def validate(self):
+        pass
+
+    def _validate_type(self, attribute_name: str, stored_attribute: Any):
+        
+        if not isinstance(stored_attribute, self.allowed_types):
+
+            raise UnexpectedAttributeTypeError(attribute_name, 
+                                               self.dataclass_validated, 
+                                               str(type(stored_attribute)), 
+                                               [str(obj) for obj in self.allowed_types])
+        
+    def _validate_length_nonzero(self, attribute_name: str, stored_attribute: Union[pd.DataFrame, Dict]):
+
+        if not len(stored_attribute) > 0:
+            raise EmptyAttributeTypeError(attribute_name, self.dataclass_validated)
+                    
