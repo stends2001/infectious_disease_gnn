44b5693956800739a5c06f6f701694f03b99bfb0
de-SchrijverS <de-schrijvers@hpc-login01.rki.local>
2026-04-09 10:16:02 +0200

    renaming normalizer > transformer

--- src/dataloading/epidataorchestration/orchestrator_helpers/__init__.py
+++ src/dataloading/epidataorchestration/orchestrator_helpers/__init__.py
@@ -2,5 +2,5 @@ from .loader import EpiDataReader
 from .harmonizer import EpiDataHarmonizer
 from .processor import EpiDataProcessor
 from .featurebuilder import EpiFeatureBuilder
-from .normalizer import EpiDataNormalizer
+from .transformer import EpiDataTransformer
 from .finalizer import EpiDataFinalizer
