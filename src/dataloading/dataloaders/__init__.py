eaaee1c5e3216cf196b6320142f6dcac617f38ef
de-SchrijverS <de-schrijvers@hpc-login01.rki.local>
2026-04-13 09:18:06 +0200

    module - level imports

--- src/dataloading/dataloaders/__init__.py
+++ src/dataloading/dataloaders/__init__.py
@@ -1,4 +1,8 @@
+from typing import TypeVar, Union
+
 from .baselineloader.baselinedataloadermanager import BaseLineDataLoaderManager
 # from .shallowloader.shallowdataloadermanager import ShallowDataLoaderManager
 from .deepdataloaders.deepdataloader import DeepDataLoaderManager 
 from .deepdataloaders.graphdataloader import GraphDataLoaderManager
+
+DLM = TypeVar('DLM', bound=Union[BaseLineDataLoaderManager, DeepDataLoaderManager, GraphDataLoaderManager])
