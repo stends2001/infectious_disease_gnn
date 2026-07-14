29dcd79789f13b88e7596ef70e10bb330f607444
de-SchrijverS <de-schrijvers@hpc-login01.rki.local>
2026-07-14 14:24:54 +0200

    included train/val/test as type

--- src/utils/types.py
+++ src/utils/types.py
@@ -4,9 +4,10 @@ from typing import Literal
 Pre-defined types: mostly Literals
 """
 
-#Country         = Literal['germany', 'hungary']
 Country         = Literal['germany','hungary']
 Level           = Literal['nuts1', 'nuts2', 'nuts3']
 GraphType       = Literal['identity','geographical_contiguity','gravity_model','random','fully_connected']
 GraphNormType   = Literal['minmax','symmetric','rowwise']
 ColumnType      = Literal['context','feature','target','pred','split']
+
+DataSetSplit    = Literal['train','val','test']
