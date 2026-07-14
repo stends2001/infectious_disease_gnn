1200c7add79a05c8fbbff67b7e78533805ff6233
de-SchrijverS <de-schrijvers@hpc-login01.rki.local>
2026-07-14 11:45:21 +0200

    improved logic and removed redundancies

--- src/dataloading/columnregistration/colentry.py
+++ src/dataloading/columnregistration/colentry.py
@@ -1,7 +1,15 @@
 from dataclasses import dataclass
-from typing import Optional, Dict, Literal
-from .issues import InvalidColEntry
+from typing import Optional, Literal
 from .transformation_params import TransformationParams
+
+class ColEntryMissingAttribute(Exception):
+    def __init__(self, entry_name: str, attribute_name: str):
+        msg = f"Accession attempt was made on ColEntry {entry_name} for attribute {attribute_name} which is unavailable."
+        super().__init__(msg)
+
+class InvalidColEntry(Exception):
+    def __init__(self, msg: str):
+        super().__init__(msg)    
 
 @dataclass
 class ColEntry:
@@ -21,47 +29,43 @@ class ColEntry:
         - transformation_group == 'self'    => individual transformation (based on parameters from itself)
         - transformation_group == {column_name} of another ColEntry
         - transformation_group == None      => only possible when self.transformation == False
-    # TODO
-    _transformation_params: ... 
     """
     column_name:            str
     column_type:            Literal['context','feature','target','pred','split']
     transformation:         bool 
-    _transformation_group:  Optional[str]            = None
-    _transformation_params: Optional[TransformationParams] = None    
+    _transformation_group:  Optional[str]                   = None
+    _transformation_params: Optional[TransformationParams]  = None    
     
     def __post_init__(self):
-        
         # lower case input
         self.column_name = self.column_name.lower()
-        self.column_type = self.column_type.lower()  # type: ignore[assignment]
+        self._validate_input()
 
+        # ensure lower case in transformation group
         if self._transformation_group:
             self._transformation_group = self._transformation_group.lower()
 
-        # validate against impossible states
-        self._validate_input()
-
-    def _validate_input(self):
-        """to be run post init: validate whether combination of input makes sense or not"""
-        allowed_types = ['context','feature','target','pred','split']
-        
-        if self.column_type not in allowed_types:
-            raise InvalidColEntry(self.column_name, f'got unsupported value for column_type ({self.column_type}). Supported values are: {allowed_types}') 
+    def _validate_input(self) -> None:
+        """validate against invalid states"""
+        if not self.transformation:
+            if self._transformation_group is not None:
+                raise InvalidColEntry(f"transformation is False, but _transformation_group is given for {self.column_name}.")
+            if self._transformation_params is not None:
+                raise InvalidColEntry(f"transformation is False, but _transformation_params is given for {self.column_name}.")                
 
     @property
     def transformation_group(self) -> str:
         if self._transformation_group:
             return self._transformation_group
         else:
-            raise ValueError(f'attempted to access transformation group for {self.column_name}')
+            raise ColEntryMissingAttribute(self.column_name, "transformation_group")
 
     @property
-    def transformation_params(self) -> Dict[str,Dict[str,dict]]:
+    def transformation_params(self) -> TransformationParams:
         if self._transformation_params:
             return self._transformation_params
         else:
-            raise ValueError(f'attempted to access transformation_params for {self.column_name}')        
+            raise ColEntryMissingAttribute(self.column_name, "transformation_params")     
     
     def __repr__(self) -> str:
         representation = (
