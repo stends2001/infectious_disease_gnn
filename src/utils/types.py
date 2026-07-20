8654cfabda09d0902f4b0fb5df0f1d7413d8378d
de-SchrijverS <de-schrijvers@hpc-login01.rki.local>
2026-07-20 09:42:06 +0200

    added graphnormtype none

--- src/utils/types.py
+++ src/utils/types.py
@@ -7,7 +7,7 @@ Pre-defined types: mostly Literals
 Country         = Literal['germany','hungary']
 Level           = Literal['nuts1', 'nuts2', 'nuts3']
 GraphType       = Literal['identity','geographical_contiguity','gravity_model','random','fully_connected']
-GraphNormType   = Literal['minmax','symmetric','rowwise']
+GraphNormType   = Literal['minmax','symmetric','rowwise','none']
 ColumnType      = Literal['context','feature','target','pred','split']
 
 DataSetSplit    = Literal['train','val','test']
