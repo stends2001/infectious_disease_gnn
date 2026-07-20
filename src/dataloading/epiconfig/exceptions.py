0b5c2ed1854f191a37ab106a67cc2b6a6df3e1f6
de-SchrijverS <de-schrijvers@hpc-login01.rki.local>
2026-07-20 16:11:59 +0200

    removed whitelines

--- src/dataloading/epiconfig/exceptions.py
+++ src/dataloading/epiconfig/exceptions.py
@@ -1,26 +1,17 @@
-
 # ======= EPICONFIG ======= #
-class EpiConfigValidationError(Exception):
-    """
-    errors are to be raised!
-    """    
+class EpiConfigValidationError(Exception):   
     def __init__(self, msg: str):
         super().__init__(msg)
 
 class EpiConfigLimitationError(Exception):
-    """
-    """    
     def __init__(self, msg: str):
         super().__init__(msg)  
 
-class InvalidCovariatePath(Exception):
-    """
-    """    
+class InvalidCovariatePath(Exception):  
     def __init__(self, covariate: str, path: str):
         msg = f"Coviariate {covariate} was not found at {path}."
         super().__init__(msg)      
 
 class IncompatibleEpiConfigs(Exception):
-
     def __init__(self, msg: str):
         super().__init__(msg)
